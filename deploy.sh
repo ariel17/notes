0fa4b6b8ad73c028dc7cdb94b03e7bf516bb2691
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-04-18 01:55:30 -0300

    Add very very simple deploy script.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Very very simple deploy script using rsync to upload generated documentation
+# into remote server.
+
+BUILD_PATH='_build/html'
+REMOTE_USER='ariel17'
+REMOTE_PATH='~/www/notes'
+
+make clean html
+rsync -avp --delete --progress -r -e ssh $BUILD_PATH/* $REMOTE_USER:$REMOTE_PATH
